e59f9ee2e92c6d9a9e58b8b51a0d790b0c62bc00
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-03-27 17:00:44 -0300

    Criação da camada de persistência de dados

---
+++ src/db/productdbhelper.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+async function saveProduct(productName: string) {
+    try {
+        
+        let key: string = new Date().getTime() + productName 
+        await AsyncStorage.setItem(key, productName);
+    } catch (error) {
+        console.error(`Failed to save product ${productName} - error: ${error}`);
+    }
+}
+
+async function getAllProducts() {
+    try {
+        AsyncStorage.getAllKeys()
+        .then((keys) => {
+            AsyncStorage.multiGet(keys)
+            .then((result) => {
+                console.log(`${JSON.stringify(result)}`);
+            })
+            .catch((error) => console.error(`Failed to get all product - error: ${error}`));
+        })
+        .catch((error) => console.error(`Failed to get all product keys - error: ${error}`));
+    } catch (error) {
+        
+    }
+}
+
+export const ProductDatabaseHelper = {
+    saveProduct,
+    getAllProducts
+};
