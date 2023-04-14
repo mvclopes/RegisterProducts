e6c1e7e919173816d220240027fe2a88d36a0cb9
Matheus Lopes <matheus.lopes23@hotmail.com>
2023-04-14 16:47:41 -0300

    Criada tela de listagem de produtos

---
+++ src/screens/ProductList/index.tsx
@@ -0,0 +1,66 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Product } from "../../models/Product";
+import { ListingNavigationProps } from "../../routes";
+
+export default function ProductList() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const navigation = useNavigation<ListingNavigationProps>();
+
+    const goToRegister = () => {
+        console.log("Go to Register");
+        navigation.replace("Register");
+    };
+
+    useEffect(() => {
+        AsyncStorage.getAllKeys((err, keys) => {
+            AsyncStorage.multiGet(keys, (err, result) => {
+                const products: Product[] = [];
+                result.map((_result, i, product) => {
+                    products.push({id: product[i][0], name: product[i][1] as string});
+                });
+                setProducts(products); 
+            });
+          });
+      }, [])
+    
+    return(
+        <>  
+            <View style={{flex:1}}>
+                <FlatList
+                    data={products}
+                    renderItem={ ({item}) => <Text>{item.name}</Text> }
+                    keyExtractor={ item => item.id }
+                />
+            </View>
+            
+            <View style={styles.floatingButton}>
+                <TouchableOpacity onPress={() => goToRegister()}>
+                    <Text style={styles.floatingButtonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    floatingButton: {   
+        backgroundColor: '#FF0000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 70,
+        height: 70,
+        position: 'absolute',
+        bottom: 24,
+        right: 24,
+        borderRadius: 100
+    },
+    floatingButtonText: {
+        fontSize: 48,
+        color: '#fff',
+        textAlign: 'center',
+    }
+});
